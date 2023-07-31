5b240e89242fea6b3b90109d5de928d6cd91800c
misprit7 <xandernaumenko@gmail.com>
2023-07-30 22:41:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">
+  flame-runner
+</h1>
+<h2 align="center">
+  An RL Self Driving Mario Kart Wii AI
+</h2>
+
---
+++ agent.py
@@ -0,0 +1,28 @@
+from controls.pad import Button, Pad, Stick, Trigger
+import cv2, pathlib, os, time
+
+dump_url = pathlib.Path('rtsp://localhost:8554/mystream')
+
+# while len(os.listdir(dump_dir)) == 0: time.sleep(0.001)
+
+time.sleep(0.5)
+
+vidcap = cv2.VideoCapture(str(dump_url))
+# last_frame_num = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)-1
+# vidcap.set(cv2.CAP_PROP_POS_FRAMES, last_frame_num-10)
+
+while True:
+    ret, image = vidcap.read()
+    # last_frame_num = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)-1
+    if not ret:
+        print('read failed!')
+        continue
+
+    cv2.imshow('image',image)
+    # cv2.waitKey(0)
+    # time.sleep(1/60)
+    time.sleep(0.001)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    # time.sleep(0.0001)
+
